eb683e73147ffeb5757b6932e07bda1cbffc822a
MatiAlevMe <inefableataraxia1@gmail.com>
2024-09-12 20:52:30 -0300

    Lectura - Frecuencias, Creacion, Calculos, Tiempo (Falta)

--- interfaz_usuario/interfaz.py
+++ interfaz_usuario/interfaz.py
@@ -1,16 +1,8 @@
-"""
-Este módulo contiene la interfaz de usuario para la aplicación KineViz.
-Incluye funciones para cargar y visualizar datos de estudios en formato CSV o TXT.
-"""
-
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from lectura_archivos.lectura import leer_archivo_csv_o_txt
 
 def crear_estudio():
-    """
-    Crea un estudio basado en los archivos cargados y opciones seleccionadas.
-    """
     nombre_estudio = entrada_nombre.get()
     if not nombre_estudio:
         messagebox.showerror("Error", "El nombre del estudio no puede estar vacío")
@@ -19,10 +11,9 @@ def crear_estudio():
     archivo_seleccionado = filedialog.askopenfilename(title="Seleccionar archivo CSV o TXT", 
                                                       filetypes=[("Archivos CSV", "*.csv"), ("Archivos TXT", "*.txt")])
     if archivo_seleccionado:
-        # Leer y procesar el archivo
-        df = leer_archivo_csv_o_txt(archivo_seleccionado)
+        # Leer y procesar el archivo con el nombre del estudio
+        leer_archivo_csv_o_txt(archivo_seleccionado, nombre_estudio)
         messagebox.showinfo("Éxito", f"Archivo {archivo_seleccionado} cargado correctamente")
-        # Aquí podrías guardar el estudio en tu carpeta local
     else:
         messagebox.showerror("Error", "No se seleccionó ningún archivo")
         
@@ -42,6 +33,5 @@ def abrir_interfaz():
     
     ventana.mainloop()
 
-# Ejecutar la interfaz
 if __name__ == "__main__":
     abrir_interfaz()
