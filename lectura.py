cbcb162a3b59ecef8069fd0cc9f4bababbccd9fd
MatiAlevMe <inefableataraxia1@gmail.com>
2025-04-08 08:47:54 -0400

    upd minor

--- lectura.py
+++
@@ -1,7 +0,0 @@
-# Este archivo ahora está vacío.
-# La lógica original ha sido refactorizada y movida al paquete 'kineviz'.
-# El procesamiento de archivos ahora se maneja principalmente en:
-# - kineviz.core.services.file_service.py
-# - kineviz.core.data_processing.file_handlers.py
-# - kineviz.core.data_processing.processors.py
-# - kineviz.core.data_processing.directory_manager.py
